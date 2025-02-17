567b14537b006f26a7bba852c428765f0b9e385d
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2025-02-16 22:50:28 -0300

    Actualizar componente Home para buscar películas por título y añadir función para obtener películas desde la API

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,32 @@
 import { useState } from "react";
-import { getPelicula } from "../utils/api"
+import { getByTitle } from "../utils/api";
+import "../styles/home.css";
 
 const Home = () => {
-    const [id, setId] = useState("");
-    const [pelicula, setPelicula] = useState({});
+  const [peliculas, setPeliculas] = useState([]);
 
-    const obtenerPelicula = async () => {
-        const data = await getPelicula(id);
-        setPelicula(data);
-    }
+  const obtenerPorTitulo = async () => {
+    const data = await getByTitle(peliculas.original_title);
+    setPeliculas(data.results);
+  }
 
-    return (
-        <div>
-            <input
-                type="number"
-                value={id}
-                onChange={(e) => setId(e.target.value)}
-            />
-            <button onClick={obtenerPelicula}>Obtener Película</button>
-
-            {pelicula ? (
-        <div>
-          <h2>{pelicula.original_title}</h2>
-          <p>{pelicula.overview}</p>
+  return (
+    <div>
+      <input type="text" onChange={(e) => setPeliculas({original_title: e.target.value})} />
+      <button onClick={obtenerPorTitulo}>Buscar</button>
+      {peliculas.length > 0 && (
+        <div className="grid-container">
+          {peliculas.map((pelicula) => (
+            <div key={pelicula.id} className="card">
+              <h1>{pelicula.original_title}</h1>
+              <img src={`https://image.tmdb.org/t/p/w200${pelicula.poster_path}`} alt={pelicula.original_title} />
+              <p>{pelicula.overview}</p>
+            </div>
+          ))}
         </div>
-      ) : (
-        <p>No se ha seleccionado una película o está cargando...</p>
       )}
     </div>
-    );
+  );
 }
 
-export default Home
+export default Home;
--- src/utils/api.js
+++ src/utils/api.js
@@ -9,3 +9,15 @@ export const getPelicula = async (id) => {
     const data = await response.json();
     return data;
 }
+
+export const getByTitle = async (title) => {
+    const url = `https://api.themoviedb.org/3/search/movie?query=${title}`;
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            "Authorization": `Bearer ${import.meta.env.VITE_TOKEN_API}`
+            }
+        });
+        const data = await response.json();
+        return data;
+    }
