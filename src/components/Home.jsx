17e62b2bbd50c5cfa92cccb57a6bcc364c012c78
Lorenzo Paredes <lorenzoparedes122@gmail.com>
2025-02-16 21:38:45 -0300

    Añadir funcionalidad para obtener detalles de una película mediante ID y actualizar la API para usar el token de entorno

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
+import { getPelicula } from "../utils/api"
+
 const Home = () => {
+    const [id, setId] = useState("");
+    const [pelicula, setPelicula] = useState({});
+
+    const obtenerPelicula = async () => {
+        const data = await getPelicula(id);
+        setPelicula(data);
+    }
+
     return (
         <div>
-            <h1>Home</h1>
+            <input
+                type="number"
+                value={id}
+                onChange={(e) => setId(e.target.value)}
+            />
+            <button onClick={obtenerPelicula}>Obtener Película</button>
+
+            {pelicula ? (
+        <div>
+          <h2>{pelicula.original_title}</h2>
+          <p>{pelicula.overview}</p>
         </div>
-    )
+      ) : (
+        <p>No se ha seleccionado una película o está cargando...</p>
+      )}
+    </div>
+    );
 }
 
 export default Home
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,11 @@
 export const getPelicula = async (id) => {
-    const url = 'https://api.themoviedb.org/3/movie/11';
+    const url = `https://api.themoviedb.org/3/movie/${id}?append_to_response=videos,images`;
     const response = await fetch(url, {
         method: 'GET',
         headers: {
-            "Authorization": `Bearer ${process.env.TOKEN_API}`
+            "Authorization": `Bearer ${import.meta.env.VITE_TOKEN_API}`
         }
     });
     const data = await response.json();
     return data;
-} 
+}
