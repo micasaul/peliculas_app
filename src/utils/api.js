0b0308bee5fd74a7e01ff381c5332779ab004d3d
micasaul <mica020804@gmail.com>
2025-02-21 21:43:45 -0300

    Add API utility functions to fetch video details for series and movies

--- src/utils/api.js
+++ src/utils/api.js
@@ -21,6 +21,30 @@ export const getByTitle = async (title) => {
         return data;
 }
 
+export const getSerieVideo = async (id) => {
+        const url = `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`
+        const response = await fetch (url, {
+                method: 'GET',
+                headers: {
+                        "Authorization": `Bearer ${import.meta.env.VITE_TOKEN_API}`
+                }
+        })
+        const data = await response.json();
+        return data
+}
+
+export const getMovieVideo = async (id) => {
+        const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`
+        const response = await fetch (url, {
+                method: 'GET',
+                headers: {
+                        "Authorization": `Bearer ${import.meta.env.VITE_TOKEN_API}`
+                }
+        })
+        const data = await response.json();
+        return data
+}
+
 export const seriesDefecto = async () => {
         const url = 'https://api.themoviedb.org/3/discover/tv?include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc';
         const response = await fetch(url, {
